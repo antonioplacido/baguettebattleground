22c9541897d7615c1d96cb1ad00567302c18b4cf
antonioplacido <aplacido1992@gmail.com>
2021-05-07 15:55:20 -0400

    add auth0 provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import { auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const store = createStore(
   rootReducer,
@@ -11,8 +15,14 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </auth0Provider>,
   document.getElementById("root")
 );
